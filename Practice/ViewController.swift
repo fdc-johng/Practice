3e076534d2ab6f5388511ea8fa1f6a788edc2d5c
John Ellie <fdc.johngo@gmail.com>
2019-08-05 10:48:05 +0800

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by FDC-MM11-Leah on 05/08/2019.
+//  Copyright © 2019 FDC John Go. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
